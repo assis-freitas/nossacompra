3220b330ab7ca8141950b7e6947b09ea2384f7a8
edinho99 <edinho.029@gmail.com>
2015-11-25 15:33:00 -0200

    tela de Itens

--- itens.php
+++ itens.php
@@ -1,75 +1,44 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8" />
-		<title>Nossa Compra</title>
-		<link rel="stylesheet" type="text/css" href="css/estilo.css" />
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-		<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
-		<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-	</head>
-	<body>
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-12">
-					<h1 id="logo">Nossa Compra</h1>
-					<div class="row">
-						<div class="col-md-3">
-							<a href="#" class="btn btn-novo"><i class="fa fa-plus-circle"></i> Novo Item</a>
-						</div>
-						<div class="col-md-offset-5 col-md-4">
-							<div class="input-group">
-								<input type="text" class="form-control" />
-								<div class="input-group-btn">
-									<button class="btn btn-primary"><i class="fa fa-search"></i></button>
-								</div>
-							</div>
-						</div>
-					</div>
-					<br>
-					<div class="row">
-						<div class="col-md-offset-2 col-md-8">
-							<div class="box">
-								<h3 class="pull-left">Lista 1</h3>
-								<p class="badge pull-right">5 Itens</p>
-								<div class="clearfix"></div>
-								<hr />
-								<table class="table">
-									<thead>
-										<tr>
-											<th>Comprado</th>
-											<th>Descrição</th>
-											<th>Quantidade</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td><input type="checkbox"></td>
-											<td>Salgadinho</td>
-											<td>3</td>
-										</tr>
-										<tr class="success">
-											<td><input type="checkbox" checked="true"></td>
-											<td>Bolacha</td>
-											<td>2</td>
-										</tr>
-										<tr>
-											<td><input type="checkbox"></td>
-											<td>Leite</td>
-											<td>3</td>
-										</tr>
-										<tr class="success">
-											<td><input type="checkbox" checked="true"></td>
-											<td>Vinho</td>
-											<td>15</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</body>
-</html>
+<?php
+session_start();
+$codigo = $_GET['cod_grupo'];
+$codigo2 = $_GET['cod_lista'];
+
+
+
+
+include("header.php");
+require_once 'util/Conexao.class.php';
+require_once 'util/funcoes.php';
+$conexao = new Conexao();
+$colunas = array("*");
+$colunas1 = array("grupo.*");
+$colunas2 = array("lista.*");
+
+$dados = $conexao->select("itens_lista", $colunas, "JOIN lista ON lista.LIS_CODIGO = itens_lista.ITE_LIS_LIS_CODIGO");
+
+$dados1 = $conexao->select("grupo", $colunas1, "where GRU_CODIGO = " . $codigo);
+
+$dados2 = $conexao->select("lista", $colunas2, "where LIS_CODIGO =  " . $codigo2);
+
+?>
+<?php if(isMember($dados1[0]["GRU_CODIGO"])): ?>
+<table>
+    <thead>
+        <tr>
+            <th><?php echo $dados2[0]['LIS_NOME']; ?></th>
+        </tr> 
+    </thead>
+
+    <tbody>
+        <tr>
+            <td><?php echo $dados[0]['ITE_LIS_DESCRICAO_ITEM']; ?></td>
+        </tr>
+    </tbody>
+</table>
+
+
+<?php
+endif;
+include("scripts.php");
+include("footer.php");
+?>
